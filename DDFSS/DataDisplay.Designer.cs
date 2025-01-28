d9e8c6667b4da554fc0554770da8bd666dc09603
ruairi212 <ruairitierney212@gmail.com>
2025-01-28 08:24:39 +0000

    basic UI

--- DDFSS/DataDisplay.Designer.cs
+++ DDFSS/DataDisplay.Designer.cs
@@ -30,12 +30,22 @@
         {
             label1 = new Label();
             label2 = new Label();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            panel3 = new Panel();
+            label3 = new Label();
+            Dashboardlabel = new Label();
+            dataGridView1 = new DataGridView();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(73, 58);
+            label1.Location = new Point(89, 14);
             label1.Name = "label1";
             label1.Size = new Size(38, 15);
             label1.TabIndex = 0;
@@ -44,22 +54,86 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(73, 125);
+            label2.Location = new Point(87, 14);
             label2.Name = "label2";
             label2.Size = new Size(38, 15);
             label2.TabIndex = 1;
             label2.Text = "label2";
             // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.Control;
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(12, 52);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(234, 37);
+            panel1.TabIndex = 4;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = SystemColors.Control;
+            panel2.Controls.Add(label2);
+            panel2.Location = new Point(262, 52);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(234, 37);
+            panel2.TabIndex = 5;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = SystemColors.Control;
+            panel3.Controls.Add(label3);
+            panel3.Location = new Point(515, 52);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(234, 37);
+            panel3.TabIndex = 6;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(109, 14);
+            label3.Name = "label3";
+            label3.Size = new Size(38, 15);
+            label3.TabIndex = 0;
+            label3.Text = "label3";
+            // 
+            // Dashboardlabel
+            // 
+            Dashboardlabel.AutoSize = true;
+            Dashboardlabel.Location = new Point(26, 9);
+            Dashboardlabel.Name = "Dashboardlabel";
+            Dashboardlabel.Size = new Size(77, 15);
+            Dashboardlabel.TabIndex = 7;
+            Dashboardlabel.Text = "DASHBOARD";
+            Dashboardlabel.Click += this.label4_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(237, 288);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(295, 150);
+            dataGridView1.TabIndex = 8;
+            // 
             // DataDisplay
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.Maroon;
+            BackColor = Color.DarkGray;
             ClientSize = new Size(800, 450);
-            Controls.Add(label2);
-            Controls.Add(label1);
+            Controls.Add(dataGridView1);
+            Controls.Add(Dashboardlabel);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
             Name = "DataDisplay";
             Text = "DataDisplay";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -68,5 +142,11 @@
 
         private Label label1;
         private Label label2;
+        private Panel panel1;
+        private Panel panel2;
+        private Panel panel3;
+        private Label label3;
+        private Label Dashboardlabel;
+        private DataGridView dataGridView1;
     }
 }
